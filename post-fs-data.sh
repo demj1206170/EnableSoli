f41bf2101363816fe0d0f1cf995d5bb8c019d4c8
demj1206170 <739480396@qq.com>
2020-03-03 12:28:39 +0800

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -5,4 +5,4 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
-setprop pixel.oslo.allowed_override true 
+setprop pixel.oslo.allowed_override 1 
